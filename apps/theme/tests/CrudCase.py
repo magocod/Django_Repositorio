149d512ee24ed461986fc22486ce7812d537bba5
desarrollador03 <desarrollador03@contactoycobranza.com>
2019-08-22 15:39:30 -0400

    theme test

---
+++ apps/theme/tests/CrudCase.py
@@ -0,0 +1,82 @@
+import json
+
+from django.test import Client, TestCase
+from django.contrib.auth.models import User
+
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
+
+from apps.theme.models import Theme
+from apps.theme.serializers import ThemeSerializer
+
+USERDATA = ('usertest', 'user@test.com', '123')
+
+class CRUDTest(TestCase):
+
+  def setUp(self):
+    # user an token
+    user = User.objects.create_user(
+      USERDATA[0],
+      USERDATA[1],
+      USERDATA[2],
+    )
+    # admin
+    user.is_staff = True
+    user.save()             
+    Token.objects.get_or_create(user= user)
+    # auth token 
+    token = Token.objects.get(user__username= USERDATA[0])
+    self.client = APIClient()
+    self.client.credentials(HTTP_AUTHORIZATION= 'Token ' + token.key)
+    # data
+    self.theme = Theme.objects.create(name= 'test', description= ' test description')
+
+  def test_create_theme(self):
+    data = {
+      'name': 'test create',
+      'description' : 'test create description'
+    }
+    response = self.client.post('/api/themes/', data)
+    response_data = json.loads(response.content)
+    serializer = ThemeSerializer(
+      Theme.objects.get(id = response_data['id']),
+    )
+    self.assertEqual(response.status_code, 201)
+    self.assertEqual(serializer.data, response_data)
+
+  def test_create_error_params(self):
+    data = {
+      'name': 'test create',
+    }
+    response = self.client.post('/api/themes/', data)
+    self.assertEqual(response.status_code, 400)
+
+  def test_create_error_duplicate(self):
+    data = {
+      'name': 'test',
+      'description' : 'test create duplicate'
+    }
+    response = self.client.post('/api/themes/', data)
+    self.assertEqual(response.status_code, 400)
+
+  def test_get_theme(self):
+    response  = self.client.get('/api/theme/' + str(self.theme.pk) + '/')
+    serializer = ThemeSerializer(
+      Theme.objects.get(id= self.theme.pk)
+    )
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(serializer.data, response.data)
+  
+  def test_update_theme(self):
+    oldvalues = ThemeSerializer(self.theme)
+    newdata = {
+      'name': 'test update',
+      'description' : 'test update description'
+    }
+    response = self.client.put('/api/theme/' + str(self.theme.pk) + '/', newdata)
+    self.assertEqual(response.status_code, 200)
+    self.assertNotEqual(oldvalues.data, response.data)
+
+  def test_delete_theme(self):
+    response = self.client.delete('/api/theme/' + str(self.theme.pk) + '/')
+    self.assertEqual(response.status_code, 204)
---
+++ apps/theme/tests/ListCase.py
@@ -0,0 +1,54 @@
+import json
+
+from django.test import Client, TestCase
+from django.urls import resolve, reverse
+from django.contrib.auth.models import User
+
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
+
+from apps.theme.models import Theme
+from apps.theme.serializers import ThemeSerializer
+
+USERDATA = ('usertest', 'user@test.com', '123')
+
+class ListTest(TestCase):
+
+  def setUp(self):
+    # user an token
+    user = User.objects.create_user(
+      USERDATA[0],
+      USERDATA[1],
+      USERDATA[2],
+    )
+    user.is_staff = True
+    user.save()             
+    Token.objects.get_or_create(user= user)
+    # auth token 
+    token = Token.objects.get(user__username= USERDATA[0])
+    self.client = APIClient()
+    self.client.credentials(HTTP_AUTHORIZATION= 'Token ' + token.key)
+    # no authenticated
+    self.noauthclient = APIClient()
+    self.noauthclient.credentials(HTTP_AUTHORIZATION= 'Token ' + '123')
+    # data
+    self.theme = Theme.objects.create(name= 'test', description= ' test description')
+
+  def test_get_all(self):
+    response = self.client.get('/api/themes/')
+    response_data = json.loads(response.content)
+    serializer = ThemeSerializer(
+      Theme.objects.all(),
+      many= True,
+    )
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(serializer.data, response_data)
+
+  def test_get_all_authenticated(self):
+    response = self.noauthclient.get('/api/tags/')
+    response_data = json.loads(response.content)
+    serializer = ThemeSerializer(
+      Theme.objects.all(),
+      many= True,
+    )
+    self.assertEqual(response.status_code, 401)
