ad8a98066b13b33318149b323b45e446154865b5
unknown <magoyeison@gmail.com>
2019-09-24 19:10:52 -0400

    pep8 module theme

--- apps/theme/models.py
+++ apps/theme/models.py
@@ -1,12 +1,19 @@
+"""
+Modelos
+"""
+
 from django.db import models
 from django.utils import timezone
 
 class Theme(models.Model):
-  name = models.CharField(max_length=100, unique=True)
-  description = models.TextField(max_length=600)
-  meta = models.TextField(null=True)
-  timestamp = models.DateTimeField(auto_now_add=True)
-  updated = models.DateTimeField(default = timezone.now)
+    """
+    ...
+    """
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(max_length=600)
+    meta = models.TextField(null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(default=timezone.now)
 
-  def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
