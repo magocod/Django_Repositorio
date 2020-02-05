b9c6b3515bea34eb037222647b2b1a40e35e9e71
unknown <magoyeison@gmail.com>
2020-02-05 17:55:18 -0400

    error capitalization

---
+++ apps/tag/views/vtag.py
@@ -0,0 +1,91 @@
+"""
+Vistas edicion tag
+"""
+
+# standard library
+from typing import Union
+
+# Django
+from django.http import Http404
+from rest_framework import status
+from rest_framework.permissions import IsAdminUser
+# third-party
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+# local Django
+from apps.tag.models import Tag
+from apps.tag.serializers import TagSerializer
+
+
+class VTagList(APIView):
+    """
+    Listar y guardar
+    """
+    permission_classes = (IsAdminUser,)
+    serializer = TagSerializer
+
+    def get(self, request, format=None):
+        """
+        ...
+        """
+        listr = Tag.objects.all()
+        response = self.serializer(listr, many=True)
+        return Response(response.data, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        """
+        ...
+        """
+        response = self.serializer(data=request.data)
+        if response.is_valid():
+            response.save()
+            return Response(response.data, status=status.HTTP_201_CREATED)
+
+        return Response(response.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class VTagDetail(APIView):
+    """
+    Busqueda, edicion, eliminacion -> id
+    """
+    permission_classes = (IsAdminUser,)
+    serializer = TagSerializer
+
+    @staticmethod
+    def get_object(pk_tag: Union[int, str]):
+        """
+        buscar tag
+        """
+        try:
+            return Tag.objects.get(pk=pk_tag)
+        except Tag.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk: Union[int, str], format=None):
+        """
+        ...
+        """
+        tag = self.get_object(pk)
+        response = self.serializer(tag)
+        return Response(response.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk: Union[int, str], format=None):
+        """
+        ...
+        """
+        tag = self.get_object(pk)
+        response = self.serializer(tag, data=request.data)
+        if response.is_valid():
+            response.save()
+            return Response(response.data, status=status.HTTP_200_OK)
+
+        return Response(response.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk: Union[int, str], format=None):
+        """
+        ...
+        """
+        tag = self.get_object(pk)
+        tag.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
---
+++ apps/user/views/vuser.py
@@ -0,0 +1,126 @@
+"""
+Edicion de usuarios
+"""
+
+# standard library
+from typing import Union
+
+from django.contrib.auth.models import User
+# Django
+from django.http import Http404
+# third-party
+from rest_framework import status
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+# local Django
+from apps.user.serializers import (
+    UserHeavySerializer, UserRegisterSerializer, UserSerializer)
+
+
+class VUserList(APIView):
+    """
+    ...
+    """
+    permission_classes = (IsAdminUser,)
+    serializer = UserHeavySerializer
+
+    def serialize_user(self, pk):
+        """
+        ...
+        """
+        try:
+            user = User.objects.get(pk=pk)
+            res = self.serializer(user)
+            return res.data
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, format=None):
+        """
+        ...
+        """
+        response = self.serializer(
+            User.objects.all().order_by('id'),
+            many=True,
+        )
+        return Response(response.data, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        """
+        ...
+        """
+        response = UserRegisterSerializer(data=request.data)
+        if response.is_valid():
+            iduser: int = response.save()
+            res = self.serialize_user(pk=iduser)
+            return Response(res, status=status.HTTP_201_CREATED)
+
+        return Response(response.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class VUserDetail(APIView):
+    """
+    ...
+    """
+    permission_classes = (IsAdminUser,)
+    serializer = UserSerializer
+
+    def get_object(self, pk_user: Union[int, str]):
+        """
+        ...
+        """
+        try:
+            return User.objects.get(pk=pk_user)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk: Union[int, str], format=None):
+        """
+        ...
+        """
+        user = self.get_object(pk)
+        response = UserHeavySerializer(user)
+        return Response(response.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk: Union[int, str], format=None):
+        """
+        ...
+        """
+        user = self.get_object(pk)
+        response = self.serializer(user, data=request.data)
+        if response.is_valid():
+            result = response.save()
+            res = UserHeavySerializer(result)
+            return Response(res.data, status=status.HTTP_200_OK)
+
+        return Response(response.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk: Union[int, str], format=None):
+        """
+        ...
+        """
+        user = self.get_object(pk)
+
+        if user.id == request.user.id:
+            return Response(
+                "can't delete himself",
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if user.is_superuser:
+            return Response(
+                'super users cannot be deleted',
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if user.is_staff:
+            if not request.user.is_superuser:
+                return Response(
+                    'user cannot delete administrators',
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
