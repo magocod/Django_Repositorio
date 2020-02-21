2fa667d976035c9d1f8608fe1dd2cc4d983042ec
unknown <magoyeison@gmail.com>
2020-02-21 19:27:15 -0400

    update version 2 -> 3

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="djrepo", version="2.0", packages=find_packages(), python_requires=">=3.7",
+    name="djrepo", version="3.0", packages=find_packages(), python_requires=">=3.7",
 )
