8965a50d82e3c7dc09f9f2731e089ac17aa093dc
unknown <magoyeison@gmail.com>
2020-03-19 13:01:24 -0400

    setup details configuration

--- setup.py
+++ setup.py
@@ -1,5 +1,23 @@
 from setuptools import find_packages, setup
 
+from djrepo import __version__
+
 setup(
-    name="djrepo", version="3.0", packages=find_packages(), python_requires=">=3.7",
+    name="djrepo",
+    version=__version__,
+    url="https://github.com/magocod/djrepo",
+    author="Yson",
+    author_email="androvirtual12@gmail.com",
+    description="Digital repository, UNEFA internship project",
+    python_requires=">=3.7",
+    packages=find_packages(),
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
 )
